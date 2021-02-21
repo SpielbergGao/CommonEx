e8a00b994afb5de65e2711fbf451d7256df10122
SpielbergGao <spielberggao@gmail.com>
2021-02-21 22:51:46 +0800

    新增 Clipboard 函数

---
+++ commonext/src/main/java/com/spielberg/commonext/ClipboardUtils.kt
@@ -0,0 +1,82 @@
+package com.spielberg.commonext
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.ClipboardManager.OnPrimaryClipChangedListener
+import android.content.Context
+
+
+/**
+ * Copy the text to clipboard.
+ *
+ * The label equals name of package.
+ *
+ * @param text The text.
+ */
+fun copyText(text: CharSequence?) {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    cm?.setPrimaryClip(ClipData.newPlainText(getApplicationByReflect()?.packageName, text))
+}
+
+/**
+ * Copy the text to clipboard.
+ *
+ * @param label The label.
+ * @param text  The text.
+ */
+fun copyText(label: CharSequence?, text: CharSequence?) {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    cm?.setPrimaryClip(ClipData.newPlainText(label, text))
+}
+
+/**
+ * Clear the clipboard.
+ */
+fun clear() {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    cm?.setPrimaryClip(ClipData.newPlainText(null, ""))
+}
+
+/**
+ * Return the label for clipboard.
+ *
+ * @return the label for clipboard
+ */
+fun getLabel(): CharSequence? {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    val des = cm?.primaryClipDescription ?: return ""
+    return des.label ?: return ""
+}
+
+/**
+ * Return the text for clipboard.
+ *
+ * @return the text for clipboard
+ */
+fun getText(): CharSequence? {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    val clip = cm?.primaryClip
+    if (clip != null && clip.itemCount > 0) {
+        val text = clip.getItemAt(0).coerceToText(getApplicationByReflect())
+        if (text != null) {
+            return text
+        }
+    }
+    return ""
+}
+
+/**
+ * Add the clipboard changed listener.
+ */
+fun addChangedListener(listener: OnPrimaryClipChangedListener?) {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    cm?.addPrimaryClipChangedListener(listener)
+}
+
+/**
+ * Remove the clipboard changed listener.
+ */
+fun removeChangedListener(listener: OnPrimaryClipChangedListener?) {
+    val cm = getApplicationByReflect()?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+    cm?.removePrimaryClipChangedListener(listener)
+}
