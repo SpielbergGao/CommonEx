a3ad7f1d5da2366b57eb05dff96d8cade50c0538
SpielbergGao <spielberggao@gmail.com>
2021-02-22 12:58:22 +0800

    新增 像素转换 函数

--- commonext/src/main/java/com/spielberg/commonext/ScreenUtil.kt
+++ commonext/src/main/java/com/spielberg/commonext/ScreenUtil.kt
@@ -1,6 +1,9 @@
 package com.spielberg.commonext
 
 import android.content.Context
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 
 fun Context.getScreenWidthExt(): Int = resources.displayMetrics.widthPixels
@@ -14,4 +17,70 @@ fun Context.getStatusBarHeightExt(): Int {
         result = resources.getDimensionPixelSize(resourceId)
     }
     return result
+}
+
+
+fun Context.dp2px(dpValue: Float): Int {
+    return (dpValue * resources.displayMetrics.density + 0.5f).toInt()
+}
+
+fun Context.px2dp(pxValue: Float): Int {
+    return (pxValue / (resources.displayMetrics.density + 0.5f)).toInt()
+}
+
+fun Context.sp2px(spValue: Float): Int {
+    return (spValue * resources.displayMetrics.scaledDensity + 0.5f).toInt()
+}
+
+fun Context.px2sp(pxValue: Float): Int {
+    return (pxValue / resources.displayMetrics.scaledDensity + 0.5f).toInt()
+}
+
+fun Fragment.dp2px(dpValue: Float): Int {
+    return getApplicationByReflect()?.dp2px(dpValue) ?: 0
+}
+
+fun Fragment.px2dp(pxValue: Float): Int {
+    return getApplicationByReflect()?.px2dp(pxValue)?: 0
+}
+
+fun Fragment.sp2px(dpValue: Float): Int {
+    return getApplicationByReflect()?.sp2px(dpValue)?: 0
+}
+
+fun Fragment.px2sp(pxValue: Float): Int {
+    return getApplicationByReflect()?.px2sp(pxValue)?: 0
+}
+
+
+fun View.px2dp(pxValue: Float): Int {
+    return getApplicationByReflect()?.px2dp(pxValue)?: 0
+}
+
+fun View.dp2px(dpValue: Float): Int {
+    return getApplicationByReflect()?.dp2px(dpValue)?: 0
+}
+
+fun View.sp2px(dpValue: Float): Int {
+    return getApplicationByReflect()?.sp2px(dpValue)?: 0
+}
+
+fun View.px2sp(pxValue: Float): Int {
+    return getApplicationByReflect()?.px2sp(pxValue) ?: 0
+}
+
+fun RecyclerView.ViewHolder.px2dp(pxValue: Float): Int {
+    return itemView.px2dp(pxValue)
+}
+
+fun RecyclerView.ViewHolder.dp2px(dpValue: Float): Int {
+    return itemView.dp2px(dpValue)
+}
+
+fun RecyclerView.ViewHolder.sp2px(dpValue: Float): Int {
+    return itemView.sp2px(dpValue)
+}
+
+fun RecyclerView.ViewHolder.px2sp(pxValue: Float): Int {
+    return itemView.px2sp(pxValue)
 }
