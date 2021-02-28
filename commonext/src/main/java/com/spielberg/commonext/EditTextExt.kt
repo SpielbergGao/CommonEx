c8a9228dad760d5f0a1dc7caa885f8d6153293bc
SpielbergGao <spielberggao@gmail.com>
2021-02-28 18:56:12 +0800

    新增 EditText 函数

--- commonext/src/main/java/com/spielberg/commonext/EditTextExt.kt
+++ commonext/src/main/java/com/spielberg/commonext/EditTextExt.kt
@@ -5,6 +5,7 @@ import android.text.*
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.TextView
 
 fun EditText.addTextChangedListenerExt(
     before: ((s: CharSequence?, start: Int, count: Int, after: Int) -> Unit)? = null,
@@ -42,4 +43,53 @@ fun View.showInputMethodExt(delay: Long) {
     postDelayed({
         showInputMethodExt()
     }, delay)
+}
+
+/**
+ * 获取文本
+ */
+fun EditText.textString(): String {
+    return this.text.toString()
+}
+/**
+ * 获取去除空字符串的文本
+ */
+fun EditText.textStringTrim(): String {
+    return this.textString().trim()
+}
+/**
+ * 文本是否为空
+ */
+fun EditText.isEmpty(): Boolean {
+    return this.textString().isEmpty()
+}
+/**
+ * 去空字符串后文本是否为空
+ */
+fun EditText.isTrimEmpty(): Boolean {
+    return this.textStringTrim().isEmpty()
+}
+/**
+ * 获取文本
+ */
+fun TextView.textString(): String {
+    return this.text.toString()
+}
+/**
+ * 获取去除空字符串的文本
+ */
+fun TextView.textStringTrim(): String {
+    return this.textString().trim()
+}
+/**
+ * 文本是否为空
+ */
+fun TextView.isEmpty(): Boolean {
+    return this.textString().isEmpty()
+}
+/**
+ * 去空字符串后文本是否为空
+ */
+fun TextView.isTrimEmpty(): Boolean {
+    return this.textStringTrim().isEmpty()
 }
