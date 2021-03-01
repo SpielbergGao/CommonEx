d1a394d7f493e8824427092e6a46652b83b78d20
SpielbergGao <spielberggao@gmail.com>
2021-03-01 22:06:54 +0800

    新增 公共 函数

---
+++ commonext/src/main/java/com/spielberg/commonext/CommonExt.kt
@@ -0,0 +1,68 @@
+package com.spielberg.commonext
+
+import android.content.ClipData
+import android.content.Context
+import android.provider.Settings
+import android.text.Html
+import android.text.Spanned
+import android.text.TextUtils
+import android.view.View
+
+
+/**
+ * 判断是否为空 并传入相关操作
+ */
+inline fun <reified T> T?.notNull(notNullAction: (T) -> Unit, nullAction: () -> Unit = {}) {
+    if (this != null) {
+        notNullAction.invoke(this)
+    } else {
+        nullAction.invoke()
+    }
+}
+
+
+/**
+ * 复制文本到粘贴板
+ */
+fun Context.copyToClipboard(text: String, label: String = "JetpackMvvm") {
+    val clipData = ClipData.newPlainText(label, text)
+    clipboardManager?.setPrimaryClip(clipData)
+}
+
+
+/**
+ * 设置点击事件
+ * @param views 需要设置点击事件的view
+ * @param onClick 点击触发的方法
+ */
+fun setOnclick(vararg views: View?, onClick: (View) -> Unit) {
+    views.forEach {
+        it?.setOnClickListener { view ->
+            onClick.invoke(view)
+        }
+    }
+}
+
+/**
+ * 设置防止重复点击事件
+ * @param views 需要设置点击事件的view集合
+ * @param interval 时间间隔 默认0.5秒
+ * @param onClick 点击触发的方法
+ */
+fun setOnclickNoRepeat(vararg views: View?, interval: Long = 500, onClick: (View) -> Unit) {
+    views.forEach {
+        it?.clickNoRepeat(interval = interval) { view ->
+            onClick.invoke(view)
+        }
+    }
+}
+
+fun String.toHtml(flag: Int = Html.FROM_HTML_MODE_LEGACY): Spanned {
+    return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+        Html.fromHtml(this, flag)
+    } else {
+        Html.fromHtml(this)
+    }
+}
+
+
