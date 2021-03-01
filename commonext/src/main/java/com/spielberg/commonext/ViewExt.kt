5e7e3c2afca15c8f4efb0452b563df08290cc615
SpielbergGao <spielberggao@gmail.com>
2021-03-01 22:08:55 +0800

    新增 公共 函数

--- commonext/src/main/java/com/spielberg/commonext/ViewExt.kt
+++ commonext/src/main/java/com/spielberg/commonext/ViewExt.kt
@@ -282,3 +282,20 @@ fun View.makeMeasureSpecExt(measureSpec: Int): Int = when (measureSpec) {
     else ->
         View.MeasureSpec.makeMeasureSpec(View.MeasureSpec.getSize(measureSpec), View.MeasureSpec.EXACTLY)
 }
+
+/**
+ * 防止重复点击事件 默认0.5秒内不可重复点击
+ * @param interval 时间间隔 默认0.5秒
+ * @param action 执行方法
+ */
+var lastClickTime = 0L
+fun View.clickNoRepeat(interval: Long = 500, action: (view: View) -> Unit) {
+    setOnClickListener {
+        val currentTime = System.currentTimeMillis()
+        if (lastClickTime != 0L && (currentTime - lastClickTime < interval)) {
+            return@setOnClickListener
+        }
+        lastClickTime = currentTime
+        action(it)
+    }
+}
