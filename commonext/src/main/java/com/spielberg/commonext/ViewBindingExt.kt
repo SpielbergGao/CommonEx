70bc32c264999b2579dda958b2ad2a113c1bc776
SpielbergGao <spielberggao@gmail.com>
2021-03-06 23:55:21 +0800

    viewbinding 加入反射异常堆栈跟踪

--- commonext/src/main/java/com/spielberg/commonext/ViewBindingExt.kt
+++ commonext/src/main/java/com/spielberg/commonext/ViewBindingExt.kt
@@ -1,4 +1,3 @@
-
 @file:JvmName("ViewBindingUtil")
 @file:Suppress("UNCHECKED_CAST", "unused")
 
@@ -22,104 +21,124 @@ import kotlin.reflect.KProperty
 
 @JvmName("inflateWithGeneric")
 fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater): VB =
-  withGenericBindingClass(this) { clazz ->
-    clazz.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
-  }
+    withGenericBindingClass(this) { clazz ->
+        clazz.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
+    }
 
 @JvmName("inflateWithGeneric")
-fun <VB : ViewBinding> Any.inflateBindingWithGeneric(layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean): VB =
-  withGenericBindingClass(this) { clazz ->
-    clazz.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
-      .invoke(null, layoutInflater, parent, attachToParent) as VB
-  }
+fun <VB : ViewBinding> Any.inflateBindingWithGeneric(
+    layoutInflater: LayoutInflater,
+    parent: ViewGroup?,
+    attachToParent: Boolean
+): VB =
+    withGenericBindingClass(this) { clazz ->
+        clazz.getMethod(
+            "inflate",
+            LayoutInflater::class.java,
+            ViewGroup::class.java,
+            Boolean::class.java
+        )
+            .invoke(null, layoutInflater, parent, attachToParent) as VB
+    }
 
 @JvmName("inflateWithGeneric")
 fun <VB : ViewBinding> Any.inflateBindingWithGeneric(parent: ViewGroup): VB =
-  inflateBindingWithGeneric(LayoutInflater.from(parent.context), parent, false)
+    inflateBindingWithGeneric(LayoutInflater.from(parent.context), parent, false)
 
 fun <VB : ViewBinding> Any.bindViewWithGeneric(view: View): VB =
-  withGenericBindingClass(this) { clazz ->
-    clazz.getMethod("bind", LayoutInflater::class.java).invoke(null, view) as VB
-  }
+    withGenericBindingClass(this) { clazz ->
+        clazz.getMethod("bind", LayoutInflater::class.java).invoke(null, view) as VB
+    }
 
 private fun <VB : ViewBinding> withGenericBindingClass(any: Any, block: (Class<VB>) -> VB): VB {
-  any.allParameterizedType.forEach { parameterizedType ->
-    parameterizedType.actualTypeArguments.forEach {
-      try {
-        return block.invoke(it as Class<VB>)
-      } catch (e: NoSuchMethodException) {
-      }
+    any.allParameterizedType.forEach { parameterizedType ->
+        parameterizedType.actualTypeArguments.forEach {
+            try {
+                return block.invoke(it as Class<VB>)
+            } catch (e: NoSuchMethodException) {
+                e.printStackTrace()
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+            } catch (e : IllegalAccessException){
+                e.printStackTrace()
+            }
+        }
     }
-  }
-  throw IllegalArgumentException("There is no generic of ViewBinding.")
+    throw IllegalArgumentException("There is no generic of ViewBinding.")
 }
 
 inline fun <reified VB : ViewBinding> Activity.binding() = lazy {
-  inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
+    inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
 }
 
 inline fun <reified VB : ViewBinding> Fragment.binding() =
-  FragmentBindingDelegate(VB::class.java)
+    FragmentBindingDelegate(VB::class.java)
 
 inline fun <reified VB : ViewBinding> Dialog.binding() = lazy {
-  inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
+    inflateBinding<VB>(layoutInflater).apply { setContentView(root) }
 }
 
 inline fun <reified VB : ViewBinding> ViewGroup.binding(attachToParent: Boolean = true): VB =
-  inflateBinding(LayoutInflater.from(context), if (attachToParent) this else null, attachToParent)
+    inflateBinding(LayoutInflater.from(context), if (attachToParent) this else null, attachToParent)
 
 inline fun <reified VB : ViewBinding> TabLayout.Tab.setCustomView(onBindView: VB.() -> Unit) {
-  customView = inflateBinding<VB>(LayoutInflater.from(parent!!.context)).apply(onBindView).root
+    customView = inflateBinding<VB>(LayoutInflater.from(parent!!.context)).apply(onBindView).root
 }
 
 inline fun <reified VB : ViewBinding> inflateBinding(layoutInflater: LayoutInflater) =
-  VB::class.java.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as VB
+    VB::class.java.getMethod("inflate", LayoutInflater::class.java)
+        .invoke(null, layoutInflater) as VB
 
 inline fun <reified VB : ViewBinding> inflateBinding(parent: ViewGroup) =
-  inflateBinding<VB>(LayoutInflater.from(parent.context), parent, false)
+    inflateBinding<VB>(LayoutInflater.from(parent.context), parent, false)
 
 inline fun <reified VB : ViewBinding> inflateBinding(
-  layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean
+    layoutInflater: LayoutInflater, parent: ViewGroup?, attachToParent: Boolean
 ) =
-  VB::class.java.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
-    .invoke(null, layoutInflater, parent, attachToParent) as VB
+    VB::class.java.getMethod(
+        "inflate",
+        LayoutInflater::class.java,
+        ViewGroup::class.java,
+        Boolean::class.java
+    )
+        .invoke(null, layoutInflater, parent, attachToParent) as VB
 
 class FragmentBindingDelegate<VB : ViewBinding>(
-  private val clazz: Class<VB>
+    private val clazz: Class<VB>
 ) : ReadOnlyProperty<Fragment, VB> {
 
-  private var isInitialized = false
-  private var _binding: VB? = null
-  private val binding: VB get() = _binding!!
+    private var isInitialized = false
+    private var _binding: VB? = null
+    private val binding: VB get() = _binding!!
 
-  @Suppress("UNCHECKED_CAST")
-  override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
-    if (!isInitialized) {
-      thisRef.viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
-        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        fun onDestroyView() {
-          _binding = null
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): VB {
+        if (!isInitialized) {
+            thisRef.viewLifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
+                @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                fun onDestroyView() {
+                    _binding = null
+                }
+            })
+            _binding = clazz.getMethod("bind", View::class.java)
+                .invoke(null, thisRef.requireView()) as VB
+            isInitialized = true
         }
-      })
-      _binding = clazz.getMethod("bind", View::class.java)
-        .invoke(null, thisRef.requireView()) as VB
-      isInitialized = true
+        return binding
     }
-    return binding
-  }
 }
 
 private val Any.allParameterizedType: List<ParameterizedType>
-  get() {
-    val genericParameterizedType = mutableListOf<ParameterizedType>()
-    var genericSuperclass = javaClass.genericSuperclass
-    var superclass = javaClass.superclass
-    while (superclass != null) {
-      if (genericSuperclass is ParameterizedType) {
-        genericParameterizedType.add(genericSuperclass)
-      }
-      genericSuperclass = superclass.genericSuperclass
-      superclass = superclass.superclass
+    get() {
+        val genericParameterizedType = mutableListOf<ParameterizedType>()
+        var genericSuperclass = javaClass.genericSuperclass
+        var superclass = javaClass.superclass
+        while (superclass != null) {
+            if (genericSuperclass is ParameterizedType) {
+                genericParameterizedType.add(genericSuperclass)
+            }
+            genericSuperclass = superclass.genericSuperclass
+            superclass = superclass.superclass
+        }
+        return genericParameterizedType
     }
-    return genericParameterizedType
-  }
