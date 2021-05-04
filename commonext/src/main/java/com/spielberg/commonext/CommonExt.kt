0f0500bba762267176f5470971361c090b9943fc
Spielberg Gao <spielberggao@gmail.com>
2021-05-04 19:07:22 +0800

    Common add percentD

--- commonext/src/main/java/com/spielberg/commonext/CommonExt.kt
+++ commonext/src/main/java/com/spielberg/commonext/CommonExt.kt
@@ -197,3 +197,18 @@ fun percentI(
     if (value <= 0) return 0
     return if (value >= max) 1 else (value / max).toInt()
 }
+
+/**
+ * 计算百分比值 ( 最大 100%)
+ * @param value 指定值
+ * @param max   最大值
+ * @return 百分比值
+ */
+fun percentD(
+    value: Int,
+    max: Int
+): Double {
+    if (max <= 0) return 0.0
+    if (value <= 0) return 0.0
+    return if (value >= max) 1.0 else value.toDouble() / max.toDouble()
+}
